4713fa8cbfde9cb0c7d2811200086580f5bd068b
Eric Nelson II <nelsoneric2011@gmail.com>
2018-10-15 11:35:47 -0700

    Created psuedo code from trivia game. And added skeleton to html page and added links.

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+// Trivia Game
+    //  Trivia questtion 
+        // Create a question array 
+            // 10 questions 
+                // Each question would have a 4 option(s) to choose from
+                    // only one correct answer in each answer array
+        // if user answers question correct
+            // show user current quiz score 
+            // move to next on to next question 
+            // alert user answer is correct
+        // if user guesses wrong
+            // show user current quiz score 
+            // move to next question 
+            // alert user answer is incorrect and display correct asnwer
+        // dislpay score 
+            // Score format will be shown "0/10" 
+                // only increase score when user answers question correct
